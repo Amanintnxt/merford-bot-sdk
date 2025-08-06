15eb03a0889a712515c14da316056f5ddc3908ca
Amanintnxt <aman.intnxt@gmail.com>
2025-08-06 18:23:29 +0530

    start testing in direct line

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\HP-2\AgentsToolkitProjects\testBot\venv'
+set -gx VIRTUAL_ENV 'C:\Users\HP-2\AgentsToolkitProjects\merford-bot-sdk\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
